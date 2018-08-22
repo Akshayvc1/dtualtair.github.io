0710f11a1fefc7d621596f755839b3ec46663e6f
Akshayvc1 <42615528+Akshayvc1@users.noreply.github.com>
2018-08-22 21:29:10 +0530

    First website upload

---
+++ hover.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".pro-img").hover(function(){
+        $(this).addClass("pro-img-lg");
+
+        }, function(){
+        $(this).removeClass("pro-img-lg");
+
+    });
+});
+
+
+
+
---
+++ scroll2.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('.slide-section').click(function(){
+        alert('clicked');
+
+    });
+
+
+
+});
